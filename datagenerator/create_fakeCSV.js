6cb1d68765d8ca187ca6eb11ac73ce87da225f7d
Austin Shin <shinaustin@gmail.com>
2018-02-03 10:40:03 -0800

    (feat/datageneration) csv data generation file

--- datagenerator/create_fakeCSV.js
+++ datagenerator/create_fakeCSV.js
@@ -3,27 +3,24 @@ const faker = require('faker');
 
 // uncomment necessary sections
 
-/*
 // for creating users csv file
+/*
 const csvWriter = createCsvWriter({
-  path: __dirname + '/file4.csv',
-  header: [{ id: 'user_id', title: 'user_id' }, { id: 'name', title: 'name' }],
+  path: __dirname + '/user4.csv',
+  header: [{ id: 'user_id', title: 'user_id' }],
 });
 
 const records = [];
 
 // adjust from 0-2.5 2.5-5.0 5.0-7.5 7.5-10
-for (let i = 0; i < 10000000; i++) {
+for (let i = 7500000; i < 10000000; i++) {
   let obj = {};
   obj['user_id']= i;
-  let name = faker.name.findName();
-  obj['name'] = name;
   records.push(obj);
 }
 */
-
-/*
 // for creating inventory csv file
+/*
 const csvWriter = createCsvWriter({
   path: __dirname + '/inventory.csv',
   header: [{ id: 'product_id', title: 'product_id'}, { id: 'name' , title: 'name' }  , { id: 'description' , title: 'description' }  , { id: 'price' , title: 'price' }  , { id: 'color' , title: 'color' }  , { id: 'size' , title: 'size' }  , { id: 'inventory' , title: 'inventory' } , { id: 'avg_rating' , title: 'avg_rating' }  , { id: 'review_count' , title: 'review_count' }  , { id: 'category' , title: 'category' }, { id: 'subcategory' , title: 'subcategory' }, { id: 'department' , title: 'department' }],
@@ -47,28 +44,33 @@ for (let i = 0; i < 3000; i++) {
     department: faker.commerce.department(),
   };
   records.push(obj);
-} */
+}
+*/
+
+// / for creating event csv file
 
 /*
-// for creating event csv file
 const csvWriter = createCsvWriter({
-  path: __dirname + '/events10.csv',
-  header: [{ id: 'event_type', title: 'event_type' }, { id: 'product_id', title: 'product_id' }, { id: 'user_id', title: 'user_id' }],
+  path: `${__dirname}/events3.csv`,
+  header: [
+    { id: 'event_type', title: 'event_type' },
+    { id: 'product_id', title: 'product_id' },
+    { id: 'user_id', title: 'user_id' },
+  ],
 });
 
 const records = [];
 const event_array = ['wishlist', 'cart', 'order', 'view', 'click'];
 // adjust from 0-2.5 2.5-5.0 5.0-7.5 7.5-10
-for (let i = 45000000; i < 50000000; i++) {
-  let obj = {
-  	event_type: event_array[Math.floor(Math.random() * 5)],
-  	product_id: Math.floor(Math.random() * 3000),
-  	user_id: Math.floor(Math.random() * 10000000),
-  }
+for (let i = 10000000; i < 15000000; i++) {
+  const obj = {
+    event_type: event_array[Math.floor(Math.random() * 5)],
+    product_id: Math.floor(Math.random() * 3000),
+    user_id: Math.floor(Math.random() * 10000000),
+  };
   records.push(obj);
 }
 */
-
 csvWriter
   .writeRecords(records) // returns a promise
   .then(() => {
