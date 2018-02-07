9f5f504247b0a4c5030683401997745d0b730a27
Austin Shin <shinaustin@gmail.com>
2018-02-06 18:13:47 -0800

    (feat/newrelic) add new relic dashboard

--- performanceTest.js
+++ performanceTest.js
@@ -2,7 +2,7 @@ const siege = require('siege');
 
 siege()
   .on(3000)
-  .for(10000)
+  .for(2000)
   .times.get('/benchmarkping')
   .get('/api/getUser', { user_id: 5 })
   .get('/api/getProduct', { product_id: 5 })
